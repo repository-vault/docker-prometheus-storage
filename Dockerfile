20002f040766e1e293f10b4c5e7803ce86976a9f
set <set>
2021-05-21 15:23:48 +0200

    Working draft

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:3
 
 WORKDIR /tmp
 ARG VERSION=2.27.1
-RUN apk add --no-cache rsync jq nano
+RUN apk add --no-cache rsync jq nano curl
 ADD install.sh .
 RUN ./install.sh
 
--- run.sh
+++ run.sh
@@ -1,39 +1,58 @@
 #!/bin/sh
 
+set -ex
+
 PROC="$$"
 LOCAL_VOLUME_PATH=${LOCAL_VOLUME_PATH:-/data/local}
 REMOTE_VOLUME_PATH=${REMOTE_VOLUME_PATH:-/data/remote}
 SNAPSHOT_INTERVAL=${SNAPSHOT_INTERVAL:-120}
 
+force_sync() {
+  echo "Replication now signal"
+  killall -SIGTERM sleep || echo "No replication task available"
+}
 
-trap "exit 1" 10
-#send "replication now signal"
-trap "killall sleep || true" 11 
+trap 'exit 1' SIGTERM
+trap 'kill ${!}; force_sync' SIGUSR1
 
 abort(){
-  echo "$@" "aborting" >&2
-  kill -10 $PROC
+  echo "$@" "aborting $PROC" >&2
+  kill -SIGTERM "$PROC"
+  wait "$PROC"
 }
 
 [ ! -d "${REMOTE_VOLUME_PATH}" ] && abort "Missing REMOTE_VOLUME_PATH"
 [ ! -d "${LOCAL_VOLUME_PATH}"  ] && abort "Missing LOCAL_VOLUME_PATH"
 
 
-echo "Initial sync from $VOLUME_REMOTE_PATH"
+echo "Initial sync from $REMOTE_VOLUME_PATH"
 rsync -av "$REMOTE_VOLUME_PATH"/ "$LOCAL_VOLUME_PATH"/
 
 
 background_sync(){
-  guid=$(curl -XPOST http://127.0.0.1:9000/api/v1/admin/tsdb/snapshot | jq -r '.data.name')
+  sleep 5 # wait for prometheus to be ready (todo, better)
+
+  while true; do
+  echo "Now running snapshot"
+  guid=$(curl -s -XPOST http://127.0.0.1:9090/api/v1/admin/tsdb/snapshot | jq -r '.data.name')
   echo "Got replication guid $guid"
   snapshot_dir="$LOCAL_VOLUME_PATH/snapshots/$guid"
-  [! -d "$snapshot_dir" ] && abort "Invalid snapshot"
-  rsync -av $snapshot_dir/ $REMOTE_VOLUME_PATH/
+  ( [ -z "$guid" ] || [ ! -d "$snapshot_dir" ] ) && abort "Invalid snapshot"
+
+  rsync -av --delete $snapshot_dir/ $REMOTE_VOLUME_PATH/
   rm -rf "$snapshot_dir"
   echo "Replication done, now sleeping ${SNAPSHOT_INTERVAL}"
-  sleep $SNAPSHOT_INTERVAL
+  sleep $SNAPSHOT_INTERVAL || true
+  done
 }
 
 background_sync &
 
-/bin/prometheus "$@"
+/bin/prometheus "$@" &
+
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!} || true
+done
