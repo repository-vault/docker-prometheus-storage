c4fa3969e2ea6d063fc5c246d0c4bdae516421db
François Leurent <131.js@leurent.email>
2021-09-01 14:02:51 +0200

    With better backup policy

--- run.sh
+++ run.sh
@@ -57,7 +57,13 @@ background_sync(){
   snapshot_dir="$LOCAL_VOLUME_PATH/snapshots/$guid"
   ( [ -z "$guid" ] || [ ! -d "$snapshot_dir" ] ) && echo -e "$trace" && abort "Invalid snapshot"
 
-  rsync -av --delete $snapshot_dir/ $REMOTE_VOLUME_PATH/
+  rm -rf "$REMOTE_VOLUME_PATH.next"
+  cp -lr "$REMOTE_VOLUME_PATH" "$REMOTE_VOLUME_PATH.next"
+  rsync -av --delete $snapshot_dir/ "$REMOTE_VOLUME_PATH.next/"
+  mv "$REMOTE_VOLUME_PATH" "$REMOTE_VOLUME_PATH.old"
+  mv "$REMOTE_VOLUME_PATH.next" "$REMOTE_VOLUME_PATH"
+  rm -rf "$REMOTE_VOLUME_PATH.old"
+
   rm -rf "$snapshot_dir"
   echo "Replication done, now sleeping ${SNAPSHOT_INTERVAL}"
   sleep $SNAPSHOT_INTERVAL || true
